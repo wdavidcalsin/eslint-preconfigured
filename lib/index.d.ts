b621e4c96e8c707583fb024e36fdb4aeb52f9f56
Willian David Calin <david2c142@gmail.com>
2023-02-27 01:07:25 -0500

    build: add build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,3 @@
 export declare function createEslintrc(): void;
-export declare function helpInstall(): void;
 export declare function installPackage(): void;
 export declare function addScriptToPackageJson(): Promise<void>;
